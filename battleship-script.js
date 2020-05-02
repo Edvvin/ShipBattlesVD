6c5c40e6a76d920420d223f43e563c9d3b5d323c
Edvvin <edvvin@mail.com>
2020-05-02 02:51:35 +0200

    Created the grid

--- battleship-script.js
+++ battleship-script.js
@@ -1,3 +1,9 @@
+var rowCnt=10, colCnt = 10;
+
+function cellClickGameSetup(row, col){
+    console.log("hello world");
+}
+
 $("document").ready(function(){
 
     //Welcome Page
@@ -11,8 +17,19 @@ $("document").ready(function(){
         }
         localStorage.setItem("P1Name", P1Name);
         localStorage.setItem("P2Name", P2Name);
+        localStorage.setItem("welcomed", true);
         window.open("./battleship-setup.html", "_self");
     });
 
     //Setup page
+    
+    $("#setUpGrid").ready(function() {
+        for(var i=0; i < rowCnt; i++){
+            $("#setUpGrid").append("<div class='gridRow'></div>");
+        }
+        for(var j=0; j < colCnt; j++){
+            $(".gridRow").append("<div class='gridCell' onclick='cellClickGameSetup()'></div>");
+        }
+    });
+
 });
